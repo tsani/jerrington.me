8b1880341c1da5f023b72b568bffd0e2b6d0f943
Jacob Errington <git@mail.jerrington.me>
2018-01-22 12:57:14 -0500

    update remote deploy script

--- remote-deploy.sh
+++ remote-deploy.sh
@@ -2,13 +2,14 @@
 
 set -e
 
-ssh -t j 'ssh blog bash' <<'EOF'
+ssh j 'ssh blog bash' <<'EOF'
 export PATH=$HOME/.local/bin:$PATH
 export LANG=en_US.UTF-8
 set -e
 cd jerrington.me
 git fetch
 git reset --hard origin/master
+git submodule update --init school
 make
 EOF
 
